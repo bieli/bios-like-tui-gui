1bce033c6998157991177582bc2876b579bdedcb
Marcin Bielak <marcin.bieli@gmail.com>
2025-10-28 17:06:55 +0100

    add GUI impl.

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ I needed desktop specific application interface UI for my project, when in one c
 This is situation, when RUST language came. We've great RATATUI and RAYLIB libraries to delivery this.
 BIOS app. was random choice, but useful in my private OS dev. practice ;-)
 
+Maybe someone would like to create simulation or game or tool with that code - feel free to reuse, this is Open Source ;-)
+
 ## How it looks like?
 
 ### TUI version
@@ -16,7 +18,7 @@ BIOS app. was random choice, but useful in my private OS dev. practice ;-)
 
 ### GUI version
 
-##### TODO
+![BIOS GUI example1](assets/bios-like-tui-gui__gui_example1.png)
 
 ## How to run?
 
--- src/gui_renderer.rs
+++ src/gui_renderer.rs
@@ -19,8 +19,6 @@ impl Renderer for GuiRenderer {
         let mut submenu_index = 0;
 
         while !rl.window_should_close() {
-
-            // Submenu items
             let submenu_items = match app.current_item() {
                 "System Info" => vec![
                     "> Manufacturer: Fujitsu",
@@ -63,6 +61,12 @@ impl Renderer for GuiRenderer {
                 if rl.is_key_pressed(KeyboardKey::KEY_DOWN) {
                     submenu_index += 1;
                 }
+
+                if rl.is_key_pressed(KeyboardKey::KEY_ENTER) {
+                    show_message = true;
+                    message_title = submenu_items[submenu_index].trim_start_matches("> ").to_string();
+                    message_body = "This is an example message.\nPress Enter to close.".to_string();
+                }
             }
 
             let mut d = rl.begin_drawing(&thread);
@@ -80,6 +84,57 @@ impl Renderer for GuiRenderer {
 
             d.draw_text("BIOS Setup Utility", 20, 20, 24, Color::BLUE);
 
+            let mut x = 40;
+            for (i, item) in app.menu_items.iter().enumerate() {
+                let color = if i == app.selected { Color::YELLOW } else { Color::DARKGRAY };
+                d.draw_text(item, x, 60, 20, color);
+                x += item.len() as i32 * 12 + 30;
+            }
+
+            if submenu_index >= submenu_items.len() {
+                submenu_index = submenu_items.len().saturating_sub(1);
+            }
+
+            let left_x = 20;
+            let left_y = 100;
+            let left_w = w / 2 - 30;
+            let left_h = h - 160;
+            d.draw_rectangle_lines(left_x, left_y, left_w, left_h, border_color);
+
+            for (i, line) in submenu_items.iter().enumerate() {
+                let color = if i == submenu_index { Color::YELLOW } else { Color::BLACK };
+                d.draw_text(line, left_x + 10, left_y + 10 + i as i32 * 24, 20, color);
+            }
+
+            let help_x = w / 2 + 10;
+            let help_y = 100;
+            let help_w = w / 2 - 40;
+            let help_h = (h - 160) / 2 - 10;
+            d.draw_rectangle_lines(help_x, help_y, help_w, help_h, border_color);
+            d.draw_text("Help", help_x + 10, help_y + 10, 20, Color::BLUE);
+            d.draw_text("Use ← → arrows to navigate", help_x + 10, help_y + 40, 18, Color::BLACK);
+            d.draw_text("Use ↑ ↓ to highlight submenu", help_x + 10, help_y + 60, 18, Color::BLACK);
+            d.draw_text("Press F1 for general help", help_x + 10, help_y + 80, 18, Color::BLACK);
+
+            let instr_y = help_y + help_h + 20;
+            d.draw_rectangle_lines(help_x, instr_y, help_w, help_h, border_color);
+            d.draw_text("Instructions", help_x + 10, instr_y + 10, 20, Color::BLUE);
+            d.draw_text("F9: Previous Values", help_x + 10, instr_y + 40, 18, Color::BLACK);
+            d.draw_text("F10: Save & Exit", help_x + 10, instr_y + 60, 18, Color::BLACK);
+            d.draw_text("ESC: Exit", help_x + 10, instr_y + 80, 18, Color::BLACK);
+
+            if show_message {
+                let box_w = 400;
+                let box_h = 120;
+                let box_x = w / 2 - box_w / 2;
+                let box_y = h / 2 - box_h / 2;
+
+                d.draw_rectangle(box_x, box_y, box_w, box_h, Color::WHITE);
+                d.draw_rectangle_lines(box_x, box_y, box_w, box_h, Color::BLUE);
+                d.draw_text(&message_title, box_x + 20, box_y + 10, 20, Color::BLUE);
+                d.draw_text(&message_body, box_x + 20, box_y + 40, 18, Color::BLACK);
+                d.draw_text("[ OK ]", box_x + box_w / 2 - 30, box_y + box_h - 30, 20, Color::YELLOW);
+            }
         }
     }
 }
